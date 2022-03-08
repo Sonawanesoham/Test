30f2d5b167424b68e984c91076715a5d34923020
Sonawanesoham <99652332+Sonawanesoham@users.noreply.github.com>
2022-03-08 15:29:05 +0530

    Create Addtion.c

---
+++ Addtion.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+  
+  int a,b,c;
+  printf("Enter the no a");
+  scanf("%d",&a);
+   printf("Enter the no b");
+  scanf("%d",&b);
+    c=a+b;
+    printf("Addtion of a&b=%d",c);
+  return 0;
+}
+    
